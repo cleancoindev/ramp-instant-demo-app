801ffd5922b1f864c8919d7799f0fd4e40d40625
Grzegorz Kapkowski <grzegorz.kapkowski@gmail.com>
2019-06-24 12:10:33 +0200

    Use env depandant url for widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import './App.css';
-import { initialize } from '@ramp-network/ramp-instant-sdk';
+import React from "react";
+import "./App.css";
+import { initialize } from "@ramp-network/ramp-instant-sdk";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <button onClick={() => initialize({ hostAppName: 'Test Dharma', swapAmount: '10', swapAsset: 'ETH', hostLogoUrl: 'http://localhost:8080/logo.png' }).show()}>Click me</button>
+      <button
+        onClick={() =>
+          initialize({
+            url: process.env.REACT_APP_URL,
+            hostAppName: "Test Dharma",
+            swapAmount: "10",
+            swapAsset: "ETH",
+            hostLogoUrl: "http://localhost:8080/logo.png"
+          }).show()
+        }
+      >
+        Click me
+      </button>
     </div>
   );
-}
+};
 
 export default App;
