838fc82b88434ca024244ddb051be28b83812c05
Maciej Bembenista <maciej.bembenista@gmail.com>
2019-07-09 11:10:25 +0200

    Update example config

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,13 @@ import { initialize } from "@ramp-network/ramp-instant-sdk";
 const App: React.FC = () => {
   return (
     <div className="App">
-      <button
-        onClick={() =>
-          initialize({
-            url: process.env.REACT_APP_URL,
-            hostAppName: "Test Dharma",
-            userAddress: "0xe2E0256d6785d49eC7BadCD1D44aDBD3F6B0Ab58",
-            swapAmount: "10",
-            swapAsset: "ETH",
-            hostLogoUrl: "http://localhost:8080/logo.png"
-          }).show()
-        }
-      >
-        Click me
-      </button>
+      <button onClick={() => initialize({
+        hostAppName: 'Maker DAO',
+        hostLogoUrl: 'https://cdn-images-1.medium.com/max/2600/1*nqtMwugX7TtpcS-5c3lRjw.png',
+        swapAsset: 'ETH',
+        swapAmount: '1500000000000000000',
+        userAddress: '0xab5801a7d398351b8be11c439e05c5b3259aec9b',
+      }).show()}>Click me</button>
     </div>
   );
 };
