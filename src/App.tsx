d72523e397d3285aa31e7710192cecc32fa0107a
Grzegorz Kapkowski <grzegorz.kapkowski@gmail.com>
2019-07-23 14:13:56 +0200

    Add two buttons ETH/DAI. Use external ruls to widget

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,37 @@ import { initialize } from "@ramp-network/ramp-instant-sdk";
 const App: React.FC = () => {
   return (
     <div className="App">
-      <button onClick={() => initialize({
-        hostAppName: 'Maker DAO',
-        hostLogoUrl: 'https://cdn-images-1.medium.com/max/2600/1*nqtMwugX7TtpcS-5c3lRjw.png',
-        swapAsset: 'ETH',
-        swapAmount: '1500000000000000000',
-        userAddress: '0xab5801a7d398351b8be11c439e05c5b3259aec9b',
-        url: 'https://ri-widget-dev.firebaseapp.com/'
-      }).show()}>Click me</button>
+      <button
+        onClick={() =>
+          initialize({
+            hostAppName: "Maker DAO",
+            hostLogoUrl:
+              "https://cdn-images-1.medium.com/max/2600/1*nqtMwugX7TtpcS-5c3lRjw.png",
+            swapAmount: "100000000000000000", // 10 ** 17
+            swapAsset: "ETH",
+            url: process.env.REACT_APP_URL,
+            userAddress: "0xe2E0256d6785d49eC7BadCD1D44aDBD3F6B0Ab58"
+          }).show()
+        }
+      >
+        Buy ETH tokens with Ramp Instant Widget
+      </button>
+
+      <button
+        onClick={() =>
+          initialize({
+            hostAppName: "Maker DAO",
+            hostLogoUrl:
+              "https://cdn-images-1.medium.com/max/2600/1*nqtMwugX7TtpcS-5c3lRjw.png",
+            swapAmount: "100000000000000000", // 10 ** 17
+            swapAsset: "0xDAI...",
+            url: process.env.REACT_APP_URL,
+            userAddress: "0xe2E0256d6785d49eC7BadCD1D44aDBD3F6B0Ab58"
+          }).show()
+        }
+      >
+        Buy DAI tokens with Ramp Instant Widget
+      </button>
     </div>
   );
 };
