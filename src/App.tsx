a3baa084af05dc46bd0c59171b17e5ad7c9a8df9
Przemek Kowalczyk <git@pkowalczyk.pl>
2019-07-26 19:12:05 +0200

    add widget link customization; please don't kill me

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { initialize } from "@ramp-network/ramp-instant-sdk";
 
+
 const App: React.FC = () => {
+  const [address, setAddress] = useState("0xe2E0256d6785d49eC7BadCD1D44aDBD3F6B0Ab58");
+  const [amount, setAmount] = useState("10000000000000000"); // 10 ** 16 = 0.01 ETH
+
+  const [addressCandidate, setAddressCandidate] = useState(address);
+  const [amountCandidate, setAmountCandidate] = useState(amount);
+
+
   return (
     <div className="App">
+      <input value={addressCandidate} onInput={e => setAddressCandidate((e.target as HTMLInputElement).value)} />
+      <label>
+      {" "} -> {address}
+      </label>
+      <br/>
+      <input value={amountCandidate} onInput={e => setAmountCandidate((e.target as HTMLInputElement).value)} />
+      {" "} -> {amount} wei
+      <label>
+      </label>
+
+      <br/>
+      <button
+        onClick={() => {
+          setAddress(addressCandidate)
+          setAmount(amountCandidate)
+        }
+        }>
+        Save custom config
+      </button>
+      <br />
+
       <button
         onClick={() =>
           initialize({
             hostAppName: "Maker DAO",
             hostLogoUrl:
               "https://cdn-images-1.medium.com/max/2600/1*nqtMwugX7TtpcS-5c3lRjw.png",
-            swapAmount: "10000000000000000", // 10 ** 16 = 0.01 ETH
+            swapAmount: amount,
             swapAsset: "ETH",
             url: process.env.REACT_APP_URL,
-            userAddress: "0xe2E0256d6785d49eC7BadCD1D44aDBD3F6B0Ab58"
+            userAddress: address
           }).show()
         }
       >
@@ -27,10 +56,10 @@ const App: React.FC = () => {
             hostAppName: "Maker DAO",
             hostLogoUrl:
               "https://cdn-images-1.medium.com/max/2600/1*nqtMwugX7TtpcS-5c3lRjw.png",
-            swapAmount: "100000000000000000", // 10 ** 17 = 0.1 token
+            swapAmount: amount,
             swapAsset: "DUMMY",
             url: process.env.REACT_APP_URL,
-            userAddress: "0xe2E0256d6785d49eC7BadCD1D44aDBD3F6B0Ab58"
+            userAddress: address
           }).show()
         }
       >
